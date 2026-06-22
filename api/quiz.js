d20430d69183fdca92d31189ea1fd1adc7c3f4f0
Kavindu Akalanka <akalankakavindu5@gmail.com>
2026-06-22 23:26:25 +0530

    Update quiz.js

--- api/quiz.js
+++ api/quiz.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
         const query = new URLSearchParams(req.query).toString();
 
         const response = await fetch(
-            "https://script.google.com/macros/s/AKfycbw8vpOi71H1ZSBVBfgh_uJyR1GPxP1yBgGq5mF3wjiXFTQLhW6UPaDA9gl8AOsooKGkBQ/exec?" +
+            "https://script.google.com/macros/s/AKfycbxNOMKQy3u8h3SFcUZce36jX6aZdwxsKXFOO9TOul2PkMCJPgXP8ZxFWDb3DG8W6gDzoQ/exec?" +
             query
         );
 
